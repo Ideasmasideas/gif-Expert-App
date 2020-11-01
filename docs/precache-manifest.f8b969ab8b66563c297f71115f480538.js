a487d74b6baedcebfed0867d52e14f2becbd8f75
Ideasmasideas <jose@ideasmasideas.com>
2020-11-01 18:15:49 +0100

    Docs agregado

---
+++ docs/precache-manifest.f8b969ab8b66563c297f71115f480538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c54a41b7000b24b48fc47b377ffa66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751a743f4711f3e10364",
+    "url": "/static/css/main.8f8a9aac.chunk.css"
+  },
+  {
+    "revision": "c4d22137cfb15265737c",
+    "url": "/static/js/2.60f52508.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.60f52508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751a743f4711f3e10364",
+    "url": "/static/js/main.fe27681e.chunk.js"
+  },
+  {
+    "revision": "851e4c3c4aa894498d54",
+    "url": "/static/js/runtime-main.8ac25dc8.js"
+  }
+]);
